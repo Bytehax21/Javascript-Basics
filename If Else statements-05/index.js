87a0eab85caba3e7008b9cdba9cbe3c85c214097
Bytehax21 <owaisniazi999@gmail.com>
2025-02-21 16:39:59 +0500

    .

---
+++ If Else statements-05/index.js
@@ -0,0 +1,18 @@
+// 1. Write a program that takes a character (number or string)
+// in a variable & checks whether the given input is a
+// number, uppercase letter or lower case letter. (Hint: ASCII
+// codes:- A=65, Z=90, a=97, z=122).
+Userinput = +prompt("Enter Num or letter");
+if (Userinput == "A") {
+    alert("Uppercase")
+     if (Userinput == "a"){
+        alert("Lowercase")
+
+    }
+}
+else if(Userinput){
+    alert("It's a Number")
+}
+//  else if () {
+    
+// }
